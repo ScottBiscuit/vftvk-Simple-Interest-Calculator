0ef1b299af1fa0d58b83f794ecdf06eb27203d1b
ScottBiscuit <scottajohnstone@gmail.com>
2022-04-27 20:14:46 +0000

    major updates but not complete

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    if (p<=0) {
+        alert("Enter a positive Amount");
+        return document.getElementById("principal").focus();
+    }
+
+    var y = document.getElementById("years").value;
+    var r = document.getElementById("rate").value;
+
+    var i = p * y * r / 100; 
+
+    var x= new Date().getFullYear();
+    var year_future = x + parseInt(y);
+
+    document.getElementById("result").innerHTML=`If you deposit <b>${p}</b>, <br> at an interest rate of <b>${r}%.</b> <br> You will receive an amount of <b>${i}</b>, <br> in the year <b>${year_future}</b><br>`;
+    document.getElementById("principal").focus();
 }
-        
+
+function read_rate() {
+    var p_rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=p_rate + "%";
+}
